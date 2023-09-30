316324e6e4db12b0ae5151dc9d39d2a0f8f7bafc
Ryash <ryash072007@gmail.com>
2023-09-30 11:25:55 +0400

    fixed pose

--- 4wd_controller.py
+++ 4wd_controller.py
@@ -11,35 +11,36 @@ cap.set(4, 650)
 
 # Pose detector variable
 mp_pose = mp.solutions.pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5)
+    min_detection_confidence=0.5, min_tracking_confidence=0.5
+)
 
 # For Debug
 debug = True
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-# Taking input from camera and getting the pose 
+# Taking input from camera and getting the pose
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         print("Can't receive camera input. Exiting...")
         break
-    
+
     # Getting the pose
     results = mp_pose.process(frame)
-    
+
     # Drawing the pose
     if results.pose_landmarks:
         if debug:
             mp_drawing.draw_landmarks(
-                image=frame,
-                landmark_list=results.pose_landmarks,
-                connections=mp_pose.POSE_CONNECTIONS,
-                landmark_draw_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    
-    cv2.imshow('Pose', frame)
-    if cv2.waitKey(1) == ord('q'):
+                frame,
+                results.pose_landmarks,
+                mp.solutions.pose.POSE_CONNECTIONS,
+                mp.solutions.drawing_styles.get_default_pose_landmarks_style(),
+            )
+
+    cv2.imshow("Pose", frame)
+    if cv2.waitKey(1) == ord("q"):
         break
 
 cap.release()
