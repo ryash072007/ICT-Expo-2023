d085dcf20c395bc28585fbf86ccce036efd52d84
Yash Raj <66545565+ryash072007@users.noreply.github.com>
2023-11-24 23:13:32 +0400

    final is final

--- final_controller.py
+++ final_controller.py
@@ -1,27 +1,30 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-from math import ceil
+from math import ceil, degrees
+
+show_video = False
 
 mp_drawing = mp.solutions.drawing_utils  # type: ignore
 mp_drawing_styles = mp.solutions.drawing_styles  # type: ignore
 mp_hands = mp.solutions.hands  # type: ignore
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 650)
+
 mp_pose = mp.solutions.pose.Pose(  # type: ignore
     min_detection_confidence=0.5, min_tracking_confidence=0.5
 )
-# For static images:
+
 mp_model = mp_hands.Hands(
-    static_image_mode=True,  # only static images
-    max_num_hands=1,  # max 2 hands detection
+    static_image_mode=False,  # only static images
+    max_num_hands=1,  # max 1 hands detection
     min_detection_confidence=0.5,
-)  # detection confidence
+)
 
 
 def wristAngles(image, points):
-    # Loop through joint sets
     a = points[0]  # First coord
     b = points[1]  # Second coord
     c = points[2]  # Third coord
@@ -29,12 +32,16 @@ def wristAngles(image, points):
     radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
         a[1] - b[1], a[0] - b[0]
     )
-    angle = radians * 180.0 / np.pi
+    angle = degrees(radians)  # need to map
     angle = psMap(angle, 120, 240, 0, 180)
     angle = ceil(angle / 10) * 10
-    # angle -= 120
-    # if angle > 180.0:
-    #     angle = 360-angle
+    angle -= 90
+    
+    if angle < -90:
+        if angle > -300:
+            angle = -90
+        elif angle < -300:
+            angle = 90
 
     cv2.putText(
         image,
@@ -46,14 +53,7 @@ def wristAngles(image, points):
         2,
         cv2.LINE_AA,
     )
-    return angle  # image
-
-
-def psMap(value, input_start, input_end, output_start, output_end):
-    slope = 1.0 * (output_end - output_start) / (input_end - input_start)
-    output = output_start + slope * (value - input_start)
-    return output
-
+    return angle, image
 
 def fingerAngles(image, results, joint_list):
     # Loop through hands
@@ -73,11 +73,10 @@ def fingerAngles(image, results, joint_list):
             radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
                 a[1] - b[1], a[0] - b[0]
             )
-            angle = np.abs(radians * 180.0 / np.pi)
+            # angle = np.abs(radians * 180.0 / np.pi)
 
-            if angle > 180.0:
-                angle = 360 - angle
 
+            angle = abs(degrees(radians))  # need to map
             cv2.putText(
                 image,
                 str(round(angle, 2)),
@@ -90,25 +89,22 @@ def fingerAngles(image, results, joint_list):
             )
     return angle  # image
 
+def psMap(value, input_start, input_end, output_start, output_end):
+    slope = 1.0 * (output_end - output_start) / (input_end - input_start)
+    output = output_start + slope * (value - input_start)
+    return output
+
+
 
 while cap.isOpened():
     _, image = cap.read()
-    wA = 0
-    tA = 0
-    # now we flip image and convert to rgb image and input to model
+    wrist_angle = 0
+    thumb_angle = 0
     image = cv2.flip(image, 1)
     point_lists = []
     results = mp_model.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     if results.multi_hand_landmarks:
-        # for hand_landmarks in results.multi_hand_landmarks:
-        #     mp_drawing.draw_landmarks(
-        #         image, # image to draw
-        #         hand_landmarks, # model output
-        #         mp_hands.HAND_CONNECTIONS, # hand connections
-        #         mp_drawing_styles.get_default_hand_landmarks_style(),
-        #         mp_drawing_styles.get_default_hand_connections_style())
-
-        tA = fingerAngles(image, results, [[4, 2, 6]])
+        thumb_angle = fingerAngles(image, results, [[6, 2, 4]])
 
         for hand in results.multi_hand_landmarks:
             a = np.array([hand.landmark[4].x, hand.landmark[4].y])
@@ -117,27 +113,29 @@ while cap.isOpened():
             point_lists.append(b)
 
     results = mp_pose.process(image)
-    # if results.pose_landmarks:
-    #     mp.solutions.drawing_utils.draw_landmarks(
-    #     image,
-    #     results.pose_landmarks,
-    #     mp.solutions.pose.POSE_CONNECTIONS,
-    #     mp.solutions.drawing_styles.get_default_pose_landmarks_style()
-    # )
-    a = results.pose_landmarks.landmark[13].x
-    b = results.pose_landmarks.landmark[13].y
-    point_lists.append(np.array([a, b]))
+
+    point_lists.append(
+        np.array(
+            [
+                results.pose_landmarks.landmark[13].x,
+                results.pose_landmarks.landmark[13].y,
+            ]
+        )
+    )
 
     if len(point_lists) == 3:
-        wA = wristAngles(image, point_lists)
-
-    if tA != 0:
-        if tA >= 70:
-            print("Accelerating")
-        else:
-            print("Decelrating")
-    cv2.imshow("Detecting poses", image)
+            wrist_angle, _ = wristAngles(image, point_lists)
+             
+    data_to_send = {
+        "turn_angle": int(wrist_angle // 10),
+        "accn": 1 if thumb_angle >= 70 else 0
+    }
+    
+    print(data_to_send)
+    
+    if show_video: cv2.imshow("Detecting angles", image)
     if cv2.waitKey(10) & 0xFF == ord("q"):
         break
+
 cap.release()
 cv2.destroyAllWindows()
